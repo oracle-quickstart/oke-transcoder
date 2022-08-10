eb6fad1097d0f7e9df248abff717b969b803fb41
mprestin77 <54962742+mprestin77@users.noreply.github.com>
2022-08-09 19:59:06 -0700

    modified:   userdata/scripts/cli_config.sh

--- userdata/scripts/cli_config.sh
+++ userdata/scripts/cli_config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# sudo curl -L -O https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh && sudo chmod a+x install.sh && sudo ./install.sh --accept-all-defaults
-curl -L -O https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh && chmod a+x install.sh && ./install.sh --accept-all-defaults
+
+#curl -L -O https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh && chmod a+x install.sh && ./install.sh --accept-all-defaults
+
+sudo yum install python36-oci-cli
 
 echo "export OCI_CLI_AUTH=instance_principal" >> ~/.bash_profile
 echo "export OCI_CLI_AUTH=instance_principal" >> ~/.bashrc
